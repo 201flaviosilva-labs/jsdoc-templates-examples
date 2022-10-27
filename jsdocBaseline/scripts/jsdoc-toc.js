f0f1351258f737a9ab24c1e97b6a84b24ee399d7
201flaviosilva <55815265+201flaviosilva@users.noreply.github.com>
2022-10-27 16:08:20 +0000

    Deploying to gh-pages from @ 201flaviosilva-labs/jsdoc-templates-examples@053b803b8ac61727ed0c7b3df024ee5b8ef478ea 🚀

--- jsdocBaseline/scripts/jsdoc-toc.js
+++ jsdocBaseline/scripts/jsdoc-toc.js
@@ -6,7 +6,7 @@
     treeNode.tree({
         autoEscape: false,
         closedIcon: '&#x21e2;',
-        data: [{"label":"<a href=\"global.html\">Globals</a>","id":"global","children":[]},{"label":"<a href=\"FIFO.html\">FIFO</a>","id":"FIFO","children":[]},{"label":"<a href=\"LIFO.html\">LIFO</a>","id":"LIFO","children":[]},{"label":"<a href=\"Maths.html\">Maths</a>","id":"Maths","children":[]},{"label":"<a href=\"Objects.html\">Objects</a>","id":"Objects","children":[{"label":"<a href=\"Objects.Circle.html\">Circle</a>","id":"Objects.Circle","children":[]},{"label":"<a href=\"Objects.ObjectBase.html\">ObjectBase</a>","id":"Objects.ObjectBase","children":[]},{"label":"<a href=\"Objects.Rectangle.html\">Rectangle</a>","id":"Objects.Rectangle","children":[]}]}],
+        data: [{"label":"<a href=\"global.html\">Globals</a>","id":"global","children":[]},{"label":"<a href=\"FIFO.html\">FIFO</a>","id":"FIFO","children":[]},{"label":"<a href=\"Games.html\">Games</a>","id":"Games","children":[]},{"label":"<a href=\"LIFO.html\">LIFO</a>","id":"LIFO","children":[]},{"label":"<a href=\"Maths.html\">Maths</a>","id":"Maths","children":[]},{"label":"<a href=\"Objects.html\">Objects</a>","id":"Objects","children":[{"label":"<a href=\"Objects.Circle.html\">Circle</a>","id":"Objects.Circle","children":[]},{"label":"<a href=\"Objects.ObjectBase.html\">ObjectBase</a>","id":"Objects.ObjectBase","children":[]},{"label":"<a href=\"Objects.Rectangle.html\">Rectangle</a>","id":"Objects.Rectangle","children":[]}]}],
         openedIcon: ' &#x21e3;',
         saveState: false,
         useContextMenu: false
